5d49c972d4134e1d8bb452fa3243c1f0c3925fa9
Weston Bertlshofer <weston@Westons-MacBook.lan>
2017-05-30 21:38:20 -0600

    Initial commit

---
+++ src/Router.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Scene, Router, Actions } from 'react-native-router-flux';
+import LoginForm from './components/LoginForm';
+import CurrentEvent from './components/currentEvent';
+import JoinEvent from './components/joinEvent';
+import CreateEvent from './components/createEvent';
+import Camera from './components/camera';
+
+
+const RouterComponent = () => {
+  return (
+    <Router sceneStyle={{ paddingTop: 65 }}>
+      <Scene key="auth">
+        <Scene key="login" component={LoginForm} title="Event Photos" />
+      </Scene>
+
+      <Scene key="main">
+        <Scene
+          onRight={() => Actions.JoinEvent()}
+          rightTitle="Join"
+          onLeft={() => Actions.CreateEvent()}
+          leftTitle="Create"
+          key="CurrentEvents"
+          component={CurrentEvent}
+          title="Current Events"
+          initial
+        />
+      </Scene>
+      <Scene key="JoinEvent" component={JoinEvent} title="Join Event" />
+      <Scene key="CreateEvent" component={CreateEvent} title="Create Event" />
+      <Scene key="Camera" component="Camera" />
+    </Router>
+  );
+};
+
+export default RouterComponent;
---
+++ src/components/currentEvent.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {Actions} from 'react-native-router-flux';
+import { Card, CardSection, Button } from './common';
+
+class CurrentEvent extends Component {
+
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Button onPress={()=>Actions.Camera()}>
+            Launch Camera here
+          </Button>  
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+
+export default CurrentEvent
